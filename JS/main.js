bb45a6b2c0e098344099b5b90cde7a31dc5adf45
Nick McFarlin <nick.f.mcfarlin@gmail.com>
2022-04-17 15:34:20 -0400

    java changes, prettier added

--- JS/main.js
+++ JS/main.js
@@ -1,32 +1,9 @@
-const scrollOffset = 100;
- 
-const scrollElement = document.querySelector("hidden");
- 
-const elementInView = (el, _scrolloffset = 0) => {
-  const elementTop = el.getBoundingClientRect().top;
- 
-  return (
-    elementTop <= 
-    ((window.innerHeight || document.documentElement.clientHeight) - offset)
-  );
-};
- 
-const displayScrollElement = () => {
-  scrollElement.classList.add('div5');
-}
- 
-const hideScrollElement = () => {
-  scrollElement.classList.remove('hidden');
-}
- 
-const handleScrollAnimation = () => {
-  if (elementInView(scrollElement, scrollOffset)) {
-      displayScrollElement();
-  } else {
-    hideScrollElement();
-  }
-}
- 
 window.addEventListener('scroll', () => {
-  handleScrollAnimation();
-})
+  console.log('Scrolled!');
+  let content = document.querySelector('hidden');
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if(contentPosition < screenPosition){
+    content.classlist.add('div5animation');
+  }
+});
