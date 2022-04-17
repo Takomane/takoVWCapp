5c56f1b2135992d7f7f068bf5c36d23d21bb1e2a
Nick McFarlin <nick.f.mcfarlin@gmail.com>
2022-04-17 16:07:59 -0400

    js work

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,9 @@
-window.addEventListener('scroll', () => {
-  console.log('Scrolled!');
-  let content = document.querySelector('hidden');
+window.addEventListener("scroll", () => {
+  console.log("Scrolled");
+  let content = document.querySelector("hidden");
   let contentPosition = content.getBoundingClientRect().top;
   let screenPosition = window.innerHeight;
-  if(contentPosition < screenPosition){
-    content.classlist.add('div5animation');
+  if (contentPosition < screenPosition) {
+    content.classlist.add("div5animation");
   }
 });
