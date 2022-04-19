ce0770adab65c44efb37d3eb98ba9b75f94b71f3
Nick McFarlin <nick.f.mcfarlin@gmail.com>
2022-04-18 20:52:21 -0400

    fixed the JS. Kind of

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,26 @@
 window.addEventListener("scroll", () => {
-  console.log("Scrolled");
-  let content = document.querySelector("hidden");
+  let content = document.querySelector(".hidden");
   let contentPosition = content.getBoundingClientRect().top;
   let screenPosition = window.innerHeight;
   if (contentPosition < screenPosition) {
-    content.classlist.add("div5animation");
+    content.classList.add("div5animation");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  let content = document.querySelector(".hidden2");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if ((contentPosition = screenPosition)) {
+    content.classList.add("div4animation");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  let content = document.querySelector(".hidden1");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight;
+  if (contentPosition > screenPosition) {
+    content.classList.add("div1animation");
   }
 });
