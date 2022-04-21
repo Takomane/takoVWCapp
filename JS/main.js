678259c085480bb2033334c42f373fde54b51af3
Nick McFarlin <nick.f.mcfarlin@gmail.com>
2022-04-20 20:17:55 -0400

    SEND IT

--- JS/main.js
+++ JS/main.js
@@ -1,26 +1,29 @@
 window.addEventListener("scroll", () => {
-  let content = document.querySelector(".hidden");
+  let content = document.querySelector(".div5 img");
   let contentPosition = content.getBoundingClientRect().top;
   let screenPosition = window.innerHeight;
   if (contentPosition < screenPosition) {
     content.classList.add("div5animation");
+    content.classList.remove("hidden");
   }
 });
 
 window.addEventListener("scroll", () => {
-  let content = document.querySelector(".hidden2");
+  let content = document.querySelector(".div4 img");
   let contentPosition = content.getBoundingClientRect().top;
   let screenPosition = window.innerHeight;
   if ((contentPosition = screenPosition)) {
     content.classList.add("div4animation");
+    content.classList.remove("hidden2");
   }
 });
 
 window.addEventListener("scroll", () => {
-  let content = document.querySelector(".hidden1");
+  let content = document.querySelector(".div1 img");
   let contentPosition = content.getBoundingClientRect().top;
   let screenPosition = window.innerHeight;
   if (contentPosition > screenPosition) {
     content.classList.add("div1animation");
+    content.classList.remove("hidden1");
   }
 });
